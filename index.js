8ddd1d49be027c51f55b9aa91e8436d41e3670a0
David Quintero <davidq@gmail.com>
2020-10-17 14:35:05 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
+
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(fileUpload());
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", (req, res) => {
+  if (!req.files) return res.status(400).send("No file uploaded");
+
+  let upfile = req.files.upfile;
+
+  upfile.mv("./uploads/" + req.files.upfile.name, function (err) {
+    if (err) return res.status(500).send(err);
+  });
+
+  const result = {
+    name: req.files.upfile.name,
+    type: req.files.upfile.mimetype,
+    size: req.files.upfile.size,
+  };
+
+  res.send(result);
+});
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, console.log(`Listening on port ${port}`));
+
+module.exports = server;
